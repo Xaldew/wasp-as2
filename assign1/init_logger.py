e19a3085b45f50b30b3cc917aeaf08333ef32202
Johan Ruuskanen <johan.ruuskanen@control.lth.se>
2018-09-25 20:51:47 +0200

    working MC simulator

---
+++ assign1/init_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+import locale
+import argparse
+import pandas as pd
+
+
+def main(csvfile, classifiers):
+    df = pd.DataFrame([[None]*len(classifiers)], columns=classifiers)
+    df.to_csv(csvfile, index=False)  
+
+if __name__ == "__main__":
+    ARGS = sys.argv[1:]
+    sys.exit(main(sys.argv[1], sys.argv[2:])) 
---
+++ assign1/run_mc_sim.bash
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Run all classifiers a set amout of times, saving the progress of each in each
+# file. Warning: will overwrite any file already present at csvfile argument
+
+csvfile="data/log.csv"
+simulations=5
+: '
+models=(
+    "knn"
+    "svm"
+    "nusvm"
+    "dtree"
+    "rdforest"
+    "adaboost"
+    "grdboost"
+    "nbayes"
+    "gaussproc"
+    "lda"
+    "qda"
+    "mlpc"
+)
+'
+models=(
+    "knn"
+    "svm"
+    "lda"
+)
+
+# Prepare the csv file
+python ./init_logger.py ${csvfile} "${models[@]}"
+
+# Run the simulation
+for sim in $(seq ${simulations})
+do
+    for mod in ${models[@]};
+    do
+        printf "Running: ${mod}\n"
+        python ./music_taste_analyzer.py training_data.csv songs_to_classify.csv \
+               --classifier "${mod}" \
+               --random \
+               --seed $RANDOM \
+               --percent 0.2 \
+               --n-iters 100 \
+               --dump ${mode}.pkl \
+               --file ${csvfile}
+    done
+done
